8f33bd53718d39e5e95ab2dd328c4ab78b223f44
vietzerg <vietanhtran.96@gmail.com>
2017-04-17 20:52:09 +0700

    Testing the new uploads for the repo :D

---
+++ Week 1/14-4.py
@@ -0,0 +1,54 @@
+from turtle import *
+'''
+import random
+
+colormode(255)
+speed(50)
+shape("turtle")
+def draw(n):
+    color_list = ["red","blue"]
+    #looping through each shape, from biggest to smallest
+    for i in range(n,2,-1):
+        pencolor(25*i, i**2, 20*i)
+        fillcolor(random.randrange(i,255),random.randrange(i,255),random.randrange(i,255))
+        #fillcolor(color_list[i % 2])
+        begin_fill()
+
+        #drawing a shape
+        for ind in range(i):
+            forward(100)
+            left(360/i)
+        for ind in range(i):
+            forward(100)
+            right(360/i)
+        for ind in range(i):
+            backward(100)
+            right(360/i)
+        for ind in range(i):
+            backward(100)
+            left(360/i) 
+        end_fill()
+    onclick(exit)
+#print (draw(9))
+'''
+
+##while True:
+##    yob = int(input("Your year of birth?"))
+##
+##    age = 2017 - yob
+##
+##    if age < 10:
+##        print ("babe")
+##    elif age < 18:
+##        print ("teen")
+##    else:
+##        print ("adwqqweqwe\nFucker\nYEAH")
+##
+##    #print ("asd")
+
+from webbrowser import *
+from time import *
+
+while True:
+    sleep(3)
+    open("https://www.youtube.com/watch?v=gTPpyWUyhSE")
