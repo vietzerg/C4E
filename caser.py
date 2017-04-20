37a6da9105eaeca0391fba156b263b8deb6db388
Viet Anh Tran (Mr.) <vietanhtran.96@gmail.com>
2017-04-21 00:51:44 +0700

    another step, almost there

--- caser.py
+++ caser.py
@@ -14,18 +14,27 @@ def caser(length):
 
     count = 0
     counts = []
+    counter_digits_combo_test = []
     for result in results:
+        number_of_o = result.count('o')
         for digit in digits:
             first_lvl_replace = result.replace('x', str(digit))
             
             counter_digits = list(digits)
             counter_digits.remove(digit)
-            for counter_digit in counter_digits:
-                second_lvl_replace = first_lvl_replace.replace('o', str(counter_digit))
-                counts.append(second_lvl_replace)
+            counter_digits_combo = list(ite.product(counter_digits, repeat = number_of_o))
+            counter_digits_combo_test.append(counter_digits_combo)
+            for a_combo in counter_digits_combo:
+                prepare_for_replace = first_lvl_replace
+                for counter_digit in list(a_combo):
+                    prepare_for_replace = prepare_for_replace.replace('o',str(counter_digit),1)
+                counts.append(prepare_for_replace)
+##            for counter_digit in counter_digits:
+##                second_lvl_replace = first_lvl_replace.replace('o', str(counter_digit))
+##                counts.append(second_lvl_replace)
         
-    #return results
-    return len(counts), results
+
+    return len(counts)
 
 #print((caser(5)))
 ##print((caser(5)[1]))
@@ -37,8 +46,17 @@ print (caser(4))
 ##dig.remove(asd)
 ##print (dig)
 
-##stringer = 'xxxoox'
-##digg = range(5)
+stringer = 'xxxoox'
+digg = range(9)
+##while stringer.find('x') != -1:
+##    for i in digg:
+##        new = stringer.replace('x',str(i),1)    
 ##for i in digg:
 ##    new = stringer.replace('x',str(i))
 ##    print (new)
+
+##pool = list(ite.product(digg,repeat=2))
+##print ((list(pool))[1])
+##for a in list((list(pool))[1]):
+##    stringer = stringer.replace('o',str(a),1)
+##print (stringer)
