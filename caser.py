b78e92580e97cda07f4230c7c1f72dc8a9d6e62e
vietzerg <vietanhtran.96@gmail.com>
2017-04-21 15:08:30 +0700

    Finishing this little project.

--- caser.py
+++ caser.py
@@ -1,10 +1,25 @@
+##2:40PM April 21, 2017:
+##
+##I gave up trying. I wished to explore the possibility of creating a counter program that executes without storing all the cases in one list, because the execution
+##time is so long.. and additionally I supposed that using a counter should reduce that required time.
+##
+##In the exploration, I managed to define a number of helper functions to aid the process of defining a duplicated case, and they worked all right.
+##
+##Eventhough I am not completely satisfied with my work so far, I am relieved that I have made this far.
+##
+##FOR ME IN THE FUTURE, IF YOU SEE THIS AGAIN, REMEMBER YOU HAD A TIME TRYING DAY AND NIGHT TO SOLVE SUCH A PROBLEM. JUST REMEMBER SO.
+
+#_____________________________________________________________________________________________________________________________________________________________________________________
+
+##THE ORIGINAL QUESTION IS: HOW MANY CAR PLATES IDs (IN THIS FORMAT: xxx-YYYY) ARE THERE SUCH THAT xxx ARE 3 CONSECUTIVE REPEATED LETTERS AND IN YYYY THERE ARE AT LEAST 2 CONSECUTIVE
+##REPEATED DIGITS?
+##
+##IN THIS FILE I JUST DEALT WITH THE SECOND "HALF" OF THE PROBLEM: AT LEAST 2 CONSECUTIVE REPEATED DIGITS IN YYYY.
+
+#_____________________________________________________________________________________________________________________________________________________________________________________
+
 import itertools as ite
 import collections
-
-##astr = 'aabbaa'
-##z = set('aaaa')
-##print (astr[0:int((len(astr)/2))])
-##print (check_symmetry(astr))
 
 # CHECK IF THE STRING IS SYMMETRIC (RIGHT-LEFT FLIP-ABLE)
 def check_symmetry(string):
@@ -16,9 +31,7 @@ def check_symmetry(string):
     else:
         return False
 
-#print (check_symmetry(astr))
-
-# RETURN A LIST OF DUPLICATED ITEMS IN A LIST CONTAINING TUPLES WITH KEYS (CAN BE DUPLICATIONS) AND VALUES
+# PRINT ELEMENTS IN A LIST OF DUPLICATED ITEMS IN A LIST CONTAINING TUPLES WITH KEYS (CAN BE DUPLICATIONS) AND VALUES
 # THIS IS JUST FOR TESTING PROCEDURE, NOT TO INCLUDE IN THE MAIN PROGRAM
 # FOR EXAMPLE, [(1,"a"),(2,"b"),(1,"z"),(1,"d"),(2,"k"),(3,"k")]
 def get_duplicate(a_list):
@@ -30,25 +43,21 @@ def get_duplicate(a_list):
             print((key,value))
     return
 
-##zzz = [(1,"a"),(2,"b"),(1,"z"),(1,"d"),(2,"k"),(3,"k")]
-##for key, value in get_duplicate(zzz).items():
-##    if len(value) > 1:
-##        print ((key, value))
-#print (get_duplicate(zzz))
-
 # CONVERT A RESULT STRING INTO ITS DOPPELGANGER, FOR EXAMPLE: xxxoox => oooxxo
-# I AM MAKING THIS BECAUSE THE PROGRAM IS MAKING DUPLICATES OTHER THAN THE SYMMETRIC ONES
+# I AM MAKING THIS BECAUSE THE PROGRAM IS MAKING DUPLICATES OTHER THAN THE SYMMETRIC ONES. THIS IS WHERE IT GETS INTRIGUING.
 def converter(stringer):
     source = {'x':'o', 'o':'x'}
     list_char = list(stringer)
     new_list = [ source[char] for char in list_char ]
     return "".join(new_list)
 
-print (converter('xxxoox'))
-
+# NOW INTO THE MAIN PROBLEM.
 def caser(length):
     digits = range(10)
     results = []
+
+    # First I try to find all possible forms of the numbers before assembling digits to the positions.
+    
     for i in range(0, length-1):
         chain = i*"o" + (length-i)*"x"
         permu = ite.permutations(chain)
@@ -57,65 +66,40 @@ def caser(length):
             if p_str.find('xx') != -1:
                 results.append(p_str)
 
-    total_case_count = 0
-    repeat_cases_count = 0
     counts = []
-    repeat_cases = []
     for result in results:
         converter(result)
         number_of_o = result.count('o')
-        for digit in digits:
-            #all_x_replace = result.replace('x', str(digit))
-            
+        for digit in digits:            
             counter_digits = list(digits)
             counter_digits.remove(digit)
             counter_digits_combo = list(ite.product(counter_digits, repeat = number_of_o))
+
+            # This generates all possible combos from the remaining 9 digits for the "o" positions
+            
             for a_combo in counter_digits_combo:
+
+            # Accessing a particular combo to assemble it into the "o" positions
+            
                 prepare_for_replace = result.replace('x', str(digit))
+
+                # First I need to replace/lock the "x" positions
+                
                 for counter_digit in list(a_combo):
                     prepare_for_replace = prepare_for_replace.replace('o',str(counter_digit),1)
-                #yield prepare_for_replace
-                if check_symmetry(prepare_for_replace):
-                    repeat_cases_count += 1
-                    repeat_cases.append(prepare_for_replace)
-                counts.append((prepare_for_replace, a_combo))
-                #counts.append(prepare_for_replace)
-                total_case_count += 1
-                
+                counts.append(prepare_for_replace)
+    return (set(counts))
+                    
+##                if check_symmetry(prepare_for_replace):
+##                    repeat_cases_count += 1
+##                    repeat_cases.append(prepare_for_replace)
+                #counts.append((prepare_for_replace, a_combo))
 
-    #return len(set(counts))
-    return get_duplicate(counts)
-    #return len(set(counts))
+##ACTUALLY THE ABOVE COMMENT LINES ARE NOT NECESSARY ANYMORE, BECAUSE I RETURN THE SET OF THE counts list, WHICH REMOVES ALL THE DUPLICATES ANYWAY. BUT I STILL WANT TO KEEP THEM HERE,
+##IN CASE SOMETHING COMES UP.
 
-#print((caser(5)))
-##print((caser(5)[1]))
-##print((caser(5)[2]))
-#print (caser(4))
-#print (caser(7))
-##for i in caser(5):
-##    print (i)
-
+##TESTING PROGRAM EXECUTION TIME
 ##import time
 ##start_time = time.time()
 ##(caser(5))
 ##print("--- %s seconds ---" % (time.time() - start_time))
-
-##dig = list(range(10))
-##asd = 5
-##dig.remove(asd)
-##print (dig)
-
-##stringer = 'xxxoox'
-##digg = range(9)
-##while stringer.find('x') != -1:
-##    for i in digg:
-##        new = stringer.replace('x',str(i),1)    
-##for i in digg:
-##    new = stringer.replace('x',str(i))
-##    print (new)
-
-##pool = list(ite.product(digg,repeat=2))
-##print ((list(pool))[1])
-##for a in list((list(pool))[1]):
-##    stringer = stringer.replace('o',str(a),1)
-##print (stringer)
