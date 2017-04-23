c8a5f0e34769ea463580af017d0f465336755e0f
vietzerg <vietanhtran.96@gmail.com>
2017-04-23 11:42:54 +0700

    Correcting bugs.

--- Week 2/sheep_herd_using_object.py
+++ Week 2/sheep_herd_using_object.py
@@ -13,16 +13,24 @@ def create_sheep_herd():
     name = input("Hello, what's your name? ")
     many = int(input("How many sheeps do you want to have? "))
     max_size = int(input("How big is the biggest sheep? "))
-    sheep_herd_created = dict(sheep_size = list(random.sample(range(1,max_size), many)), month = 0, owner = name)
+    sheep_herd_created = dict(sheep_size = [ random.randrange(1,max_size) for i in range(many) ], month = 0, owner = name)
     print ("\nHello, {}, your sheep herd has been generated as follows\n".format(name),sheep_herd_created)
     return sheep_herd_created
 
 def shear_sheeps(your_sheep_herd):
     max_sheep = max(your_sheep_herd["sheep_size"])
     print ("\nHello, my name is {} and these are my sheep sizes:\n".format(your_sheep_herd["owner"]),your_sheep_herd["sheep_size"])
-    print ("Now my biggest sheep has size",max_sheep,"let's shear it!")
-    your_sheep_herd["sheep_size"][your_sheep_herd["sheep_size"].index(max_sheep)] = 8
-    print ("After the initial sheering, here is my flock\n",your_sheep_herd["sheep_size"],"\n")
+    print ("Now my biggest sheep(s) has size",max_sheep,"let's shear them!")
+    if max_sheep > 8:
+        
+        # In case there are many sheeps with the maximum size
+        
+        for index, sheep in enumerate(your_sheep_herd["sheep_size"]):
+            if sheep == max_sheep:
+                your_sheep_herd["sheep_size"][index] = 8
+        print ("After the initial sheering, here is my flock\n",your_sheep_herd["sheep_size"],"\n")
+    else:
+        print ("Oops. Sheeps in your herd are too small to shear!")
     return your_sheep_herd
 
 def grow(your_sheep_herd):
@@ -33,7 +41,7 @@ def grow(your_sheep_herd):
 
 def sell_sheeps(your_sheep_herd):
     sum_sheep = sum(your_sheep_herd["sheep_size"])    
-    print ("My flock has size in total: {}".format(sum_sheep))
+    print ("\nMy flock has size in total: {}".format(sum_sheep))
     print ("I would get {0} * 2$ = {1}$".format(sum_sheep,sum_sheep*2))
     
 
@@ -43,4 +51,5 @@ shear_sheeps(sheep_herd)
 grow(sheep_herd)
 grow(sheep_herd)
 grow(sheep_herd)
+sell_sheeps(sheep_herd)
 print (sheep_herd)
