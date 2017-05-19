00919a21dca3a2c8d9b4dca16007b0a86e7e42dd
vietzerg <vietanhtran.96@gmail.com>
2017-05-19 11:26:34 +0700

    Try to combine Console + GUI

--- Sokoban_GUI/main.py
+++ Sokoban_GUI/main.py
@@ -1,31 +1,37 @@
 import pygame
+import main_classes
 
+# INITIALIZE GAME
+map1 = main_classes.Map(main_classes.csv_reader("game_cfg.csv"))
 pygame.init()
 
+#screen = pygame.display.set_mode((map1.width * 10, map1.height * 10))
 screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("SokobanDCM")
 loop = True
+
 mario_image = pygame.image.load("sprites/mario.png")
 square_image = pygame.image.load("sprites/square.png")
 box_image = pygame.image.load("sprites/box.png")
+storage_image = pygame.image.load("sprites/gate.png")
 
 square_width = square_image.get_width()
 square_height = square_image.get_height()
 
-col_count = 5
-row_count = 5
+col_count = map1.width
+row_count = map1.height
 
-# mario_x = 50
-# mario_y = 100
-mario_row = 0
-mario_col = 0
+mario_row = map1.chaien.y
+mario_col = map1.chaien.x
 next_mario_row = 0
 next_mario_col = 0
+move = 0
 
-box_row = 3
-box_col = 3
+# WILL BE REPLACED BY A LIST OF BOXES
+#box_row = 3
+#box_col = 3
 
-BLACK = (250, 250 ,250)
+COLOR = (250, 250 ,250)
 clock = pygame.time.Clock()
 time = 0
 
@@ -40,62 +46,37 @@ while loop:
 			loop = False
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
-				up_pressed = True
+				#up_pressed = True
+				move = "W"
 			if event.key == pygame.K_DOWN:
-				down_pressed = True
+				#down_pressed = True
+				move = "S"
 			if event.key == pygame.K_LEFT:
-				left_pressed = True
+				#left_pressed = True
+				move = "A"
 			if event.key == pygame.K_RIGHT:
-				right_pressed = True
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				right_pressed = False
-			if event.key == pygame.K_LEFT:
-				left_pressed = False
-			if event.key == pygame.K_UP:
-				up_pressed = False
-			if event.key == pygame.K_DOWN:
-				down_pressed = False
-
-	time += 1
-	if time > 5:
-		time = 0
-		# NEXT MARIO POSITION
-		if right_pressed:
-			d_row, d_col = 0, 1
-			next_mario_col, next_mario_row = min(mario_col + d_col, col_count - 1), mario_row + d_row
-		elif left_pressed:
-			d_row, d_col = 0, -1
-			next_mario_col, next_mario_row = max(mario_col + d_col, 0), mario_row + d_row
-		elif up_pressed:
-			d_row, d_col = -1, 0
-			next_mario_row, next_mario_col = max(mario_row + d_row, 0), mario_col + d_col
-		elif down_pressed:
-			d_row, d_col = 1, 0
-			next_mario_row, next_mario_col = min(mario_row + d_row, row_count - 1), mario_col + d_col
+				#right_pressed = True
+				move = "D"
+		# elif event.type == pygame.KEYUP:
+		# 	if event.key == pygame.K_RIGHT:
+		# 		right_pressed = False
+		# 	if event.key == pygame.K_LEFT:
+		# 		left_pressed = False
+		# 	if event.key == pygame.K_UP:
+		# 		up_pressed = False
+		# 	if event.key == pygame.K_DOWN:
+		# 		down_pressed = False
 
 
-	# CHECK IF MARIO PUSHES BOX
-	if (next_mario_row, next_mario_col) == (box_row, box_col):
-		if d_col == 1:			
-			box_col = min(box_col + d_col, col_count - 1)
-			mario_col = min(next_mario_col, box_col - d_col)
-		elif d_col == -1:			
-			box_col = max(box_col + d_col, 0)
-			mario_col = max(next_mario_col, box_col - d_col)
-		elif d_row == 1:			
-			box_row = min(box_row + d_row, row_count - 1)
-			mario_row = min(next_mario_row, box_row - d_row)
-		elif d_row == -1:		
-			box_row = max(box_row + d_row, 0)
-			mario_row = max(next_mario_row, box_row - d_row)
-	else:
-		mario_row = next_mario_row
-		mario_col = next_mario_col
+	# GAME LOGIC PROCESSING
+			map1.process_input(move)
+			mario_col = map1.chaien.x
+			mario_row = map1.chaien.y
 
 
 	# RE-DRAW THE MAP
-	screen.fill(BLACK)
+	screen.fill(COLOR)
+
 	for row in range(col_count):
 		for col in range(row_count):
 			#row, col
@@ -106,12 +87,49 @@ while loop:
 	mario_x = mario_col * square_width - square_width /2 + 20
 	mario_y = mario_row * square_height - square_height /2 + 20
 
-	box_x = box_col * square_width - square_width /2 + 20
-	box_y = box_row * square_height - square_height /2 + 20
-
-	#screen.blit(square_image, (20,20))
 	screen.blit(mario_image, (mario_x, mario_y))
-	screen.blit(box_image, (box_x, box_y))
+
+	for box in map1.boxes:
+		box_x = box.x * square_width - square_width /2 + 20
+		box_y = box.y * square_height - square_height /2 + 20
+		screen.blit(box_image, (box_x, box_y))
+
 	pygame.display.flip()
 	clock.tick(60)
 pygame.quit()
+
+	# time += 1
+	# if time > 5:
+	# 	time = 0
+	# NEXT MARIO POSITION
+	# if right_pressed:
+	# 	d_row, d_col = 0, 1
+	# 	next_mario_col, next_mario_row = min(mario_col + d_col, col_count - 1), mario_row + d_row
+	# elif left_pressed:
+	# 	d_row, d_col = 0, -1
+	# 	next_mario_col, next_mario_row = max(mario_col + d_col, 0), mario_row + d_row
+	# elif up_pressed:
+	# 	d_row, d_col = -1, 0
+	# 	next_mario_row, next_mario_col = max(mario_row + d_row, 0), mario_col + d_col
+	# elif down_pressed:
+	# 	d_row, d_col = 1, 0
+	# 	next_mario_row, next_mario_col = min(mario_row + d_row, row_count - 1), mario_col + d_col
+
+
+	# # CHECK IF MARIO PUSHES BOX
+	# if (next_mario_row, next_mario_col) == (box_row, box_col):
+	# 	if d_col == 1:			
+	# 		box_col = min(box_col + d_col, col_count - 1)
+	# 		mario_col = min(next_mario_col, box_col - d_col)
+	# 	elif d_col == -1:			
+	# 		box_col = max(box_col + d_col, 0)
+	# 		mario_col = max(next_mario_col, box_col - d_col)
+	# 	elif d_row == 1:			
+	# 		box_row = min(box_row + d_row, row_count - 1)
+	# 		mario_row = min(next_mario_row, box_row - d_row)
+	# 	elif d_row == -1:		
+	# 		box_row = max(box_row + d_row, 0)
+	# 		mario_row = max(next_mario_row, box_row - d_row)
+	# else:
+	# 	mario_row = next_mario_row
+	# 	mario_col = next_mario_col
---
+++ Sokoban_GUI/main_classes.py
@@ -0,0 +1,267 @@
+import csv
+       
+def csv_reader(config_file):
+    csv_read = {
+    "obstacles":[],
+    "boxes":[],
+    "storages":[],
+    "blinks":[],
+    "objects":[]
+    }
+    with open(config_file, "r") as csvfile:
+            csvreader = csv.DictReader(csvfile)
+            for row in csvreader:
+                if row["type"] == "height":
+                    csv_read["height"] = int(row["value"])
+                elif row["type"] == "width":
+                    csv_read["width"] = int(row["value"])
+                elif row["type"] == "obstacle":
+                    a_obstacle = SKObject(int(row["x"]), int(row["y"]), " O ")
+                    csv_read["obstacles"].append(a_obstacle)
+                    csv_read["objects"].append(a_obstacle)
+                elif row["type"] == "box":
+                    a_box = SKObject(int(row["x"]), int(row["y"]), " B ")
+                    csv_read["boxes"].append(a_box)
+                    csv_read["objects"].append(a_box)
+                elif row["type"] == "storage":
+                    a_storage = SKObject(int(row["x"]), int(row["y"]), " S ")
+                    csv_read["storages"].append(a_storage)
+                    csv_read["objects"].append(a_storage)
+                elif row["type"] == "character":
+                    csv_read["chaien"] = SKObject(int(row["x"]), int(row["y"]), " C ")
+                    csv_read["objects"].append(csv_read["chaien"])
+                elif row["type"] == "blink":
+                    a_blink = SKObject(int(row["x"]), int(row["y"]), " - ")
+                    csv_read["blinks"].append(a_blink)
+                    csv_read["objects"].append(a_blink)
+
+    return csv_read
+
+#print (csv_reader("game_cfg.csv")) 
+
+class SKObject:
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.char = char
+    
+    # CHECK IF THE MAP SHOULD PRINT THE OBJECT AT LOCATION (x,y)
+    def printer(self, x, y, chaien_x, chaien_y):
+        if x == self.x and y == self.y:
+            # THIS PRINTER GIVES PRIORITY FOR THE CHARACTER, BECAUSE HE CAN MOVE THROUGH SOME OBJECTS
+            if (x, y) != (chaien_x, chaien_y):
+                print (self.char, end="")
+            if (x, y) == (chaien_x, chaien_y):
+                print (" C ", end ="")
+            return True
+    
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+    
+    # GET THE NEXT POSTIION, NOT MOVE YET. USED FOR CHECKING CONDITIONS
+    def next_pos(self, dx, dy):
+        nextx = self.x + dx
+        nexty = self.y + dy
+        return (nextx, nexty)
+    
+    
+class Map:
+    def __init__(self, csv_read):
+        
+        # SETTING UP THE PROPERTIES OF THE MAP
+        self.mana = 0
+        self.obstacles = csv_read["obstacles"]
+        self.boxes = csv_read["boxes"]
+        self.storages = csv_read["storages"]
+        self.blinks = csv_read["blinks"]
+        self.objects = csv_read["objects"]
+        self.chaien = csv_read["chaien"]
+        self.height = csv_read["height"]
+        self.width = csv_read["width"]
+        
+    # CHECK IF THE (x,y) LOCATION IS AT ANY MAP OBJECT
+    # CHECK IF THERE IS ANY OBJECT PRINTED !!!
+    def print_objects(self, x, y):
+        for obj in self.objects:
+            if obj.printer(x, y, self.chaien.x, self.chaien.y):
+                return True
+        return False
+    
+    # PRINT MAP
+    def printer(self):        
+        for y in range(self.height):
+            for x in range(self.width):
+                if not self.print_objects(x, y): # IF NO OJBECT IS PRINTED, THEN...
+                    print (" - ", end ="")
+            print()
+            
+    # CHECK IF THE POSTIION (x,y) IS INSIDE THE MAP
+    def in_map(self, position_tuple):
+        if position_tuple[1] >= 0 and position_tuple[1] < self.height and position_tuple[0] >= 0 and position_tuple[0] < self.width:
+            return True
+        else:
+            return False
+    
+    # CHECK IF CHARACTER HITS AN OBJECT SERIES (SERIES OF BOXES, OBSTACLES, BLINK-POWERUPS)    
+    def in_object_series(self, that_object_list, position_tuple):
+        for obj in that_object_list:
+            if obj.x == position_tuple[0] and obj.y == position_tuple[1]:
+                return obj
+        return False
+
+    # GET A LIST BOXES TO MOVE (IN CASE CHARACTER PUSHES MULTIPLE BOXES)
+    def get_boxes_to_move(self, box_pushed, dx, dy):
+        boxes_to_move = [box_pushed]
+        
+        if dx == 1:
+            for box in self.boxes:
+                if box.x == boxes_to_move[-1].x + 1 and box.y == boxes_to_move[-1].y:
+                    boxes_to_move.append(box)
+        elif dx == -1:
+            for box in reversed(self.boxes):
+                if box.x == boxes_to_move[-1].x - 1 and box.y == boxes_to_move[-1].y:
+                    boxes_to_move.append(box)
+        elif dy == 1:
+            for box in self.boxes:
+                if box.y == boxes_to_move[-1].y + 1 and box.x == boxes_to_move[-1].x:
+                    boxes_to_move.append(box)
+        elif dy == -1:
+            for box in reversed(self.boxes):
+                if box.y == boxes_to_move[-1].y - 1 and box.x == boxes_to_move[-1].x:
+                    boxes_to_move.append(box)
+        
+        return boxes_to_move        
+        
+    # MOVE MULTIPLE BOXES
+    def move_boxes(self, boxes_to_move, dx, dy):
+        for box in boxes_to_move:
+            box.move(dx, dy)            
+    
+    # CHECK IF A BOX IS IN A STORAGE POINT
+    def in_storage(self):
+        for box in self.boxes:
+            for storage in self.storages:
+                if (box.x, box.y) == (storage.x, storage.y):
+                    return box
+        return False
+    
+    # REMOVE AN OBJECT (A BOX)
+    def remover(self, that_object_list, object_to_remove):
+        self.objects.remove(object_to_remove)
+        that_object_list.remove(object_to_remove)
+        del object_to_remove
+
+    # CHECK IF CHARACTER BLINKED
+    def check_blink(self, dx, dy):
+        if (dx,dy) == (0,-3) or (dx,dy) == (0,3) or (dx,dy) == (-3,0) or (dx,dy) == (3,0):
+            return True
+        else:
+            return False
+    
+    # MAIN PROGRAM    
+    def process_input(self, move):
+        #move = input("Your move? ").upper()
+        dx = 0
+        dy = 0
+
+        if move == "W":
+            dx,dy = 0,-1
+            dx_box,dy_box = dx,dy
+        elif move == "S":
+            dx,dy = 0,1
+            dx_box,dy_box = dx,dy
+        elif move == "A":
+            dx,dy = -1,0
+            dx_box,dy_box = dx,dy
+        elif move == "D":
+            dx,dy = 1,0
+            dx_box,dy_box = dx,dy
+        elif move == "BW":
+            if self.mana > 0:
+                dx,dy = 0,-3
+            else:
+                dx,dy = 0,-1
+                print ("Not enough mana! Moved one step instead")
+            dx_box,dy_box = 0,-1            
+        elif move == "BS":
+            if self.mana > 0:
+                dx,dy = 0,3
+            else:
+                dx,dy = 0,1
+                print ("Not enough mana!! Moved one step instead")
+            dx_box,dy_box = 0,1
+        elif move == "BA":
+            if self.mana > 0:
+                dx,dy = -3,0
+            else:
+                dx,dy = -1,0
+                print ("Not enough mana!! Moved one step instead")
+            dx_box,dy_box = -1,0
+        elif move == "BD":
+            if self.mana > 0:
+                dx,dy = 3,0
+            else:
+                dx,dy = 1,0
+                print ("Not enough mana!! Moved one step instead.")
+            dx_box,dy_box = 1,0          
+        else:
+            print ("You can choose among (W,A,S,D) !")
+            
+        C_next = self.chaien.next_pos(dx, dy)
+        
+        # CHECK IF CHARACTER TRIES TO PUSH A BOX AND MOVE MULTIPLE/SINGLE BOX/BOXES
+        if self.in_object_series(self.boxes,C_next) is not False:
+            box_pushed = self.in_object_series(self.boxes, C_next)
+            boxes_to_move = self.get_boxes_to_move(box_pushed, dx_box, dy_box)
+            last_box_next = boxes_to_move[-1].next_pos(dx_box ,dy_box)
+            
+            # CHECK IF THE LAST BOX IN THE BOX SERIES IS IN MAP AND HITS AN OBSTACLE
+            if self.in_map((last_box_next[0], last_box_next[1])) and self.in_object_series(self.obstacles, (last_box_next[0], last_box_next[1])) == False: # IF THE LAST BOX DOES NOT HIT ANYTHING, THEN...
+                    self.move_boxes(boxes_to_move, dx_box, dy_box)
+                    self.chaien.move(dx ,dy)
+                    # CHECK IF THE MOVE WAS BLINK OR NOT
+                    if self.check_blink(dx, dy):
+                        self.mana -= 1
+            else:
+                print ("Unable to move box(es).")
+        
+        # CHECK IF C NEXT HITS AN OBSTACLE
+        elif self.in_object_series(self.obstacles, C_next) != False:
+            pass
+        
+        # CHECK IF CHARACTER IS IN MAP
+        elif self.in_map(C_next):
+            self.chaien.move(dx, dy)
+            # CHECK IF THE MOVE WAS BLINK OR NOT
+            if self.check_blink(dx, dy):
+                self.mana -= 1
+            
+        else:
+            print ("Can't move out of map!")
+            
+        # CHECK IF CHARACTER EATS BLINK
+        if self.in_object_series(self.blinks, (self.chaien.x, self.chaien.y)) != False:
+            self.mana = 3
+            blink_to_remove = self.in_object_series(self.blinks, (self.chaien.x, self.chaien.y))
+            self.remover(self.blinks, blink_to_remove)
+            print ("You got a blink-powerup! 3 steps per move for 3 moves!\nPress b + a direction button to blink!")
+        
+        # CHECK IF A BOX IS IN THE STORAGE POINT
+        if self.in_storage() != False:
+            box_to_remove = self.in_storage()
+            self.remover(self.boxes, box_to_remove)
+        
+        if self.boxes == []:
+            print ("You win!")
+            return False
+        
+        print ("Your mana:",self.mana)     
+
+    
+# map1 = Map(csv_reader("game_cfg.csv"))
+# while True:
+#     map1.printer()
+#     run = map1.process_input()
+#     if run == False:
+#         break
